1a976019bd00c5ab0e815d4f19730afcfe8e1b20
Luca Casonato <lucacasonato@yahoo.com>
2019-12-21 01:04:51 +0100

    Updated example

---
+++ example/api/version.ts
@@ -0,0 +1,11 @@
+import { Context, Event,  } from "https://deno.land/x/lambda/mod.ts";
+
+export async function handler(event: Event, context: Context) {
+  return {
+    statusCode: 200,
+    body: `Welcome to deno ${Deno.version.deno} 🦕`,
+    headers: {
+      "Content-Type": "text/html; charset=utf-8"
+    }
+  };
+}
