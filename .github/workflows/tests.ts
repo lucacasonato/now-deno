69358dd105a9b9b9d3206a588bac69cb3ebda876
Luca Casonato <lucacasonato@yahoo.com>
2020-02-03 14:39:58 +0100

    Added now deploy test

---
+++ .github/workflows/tests.ts
@@ -0,0 +1,25 @@
+import {
+  test,
+  assert,
+  assertStrContains,
+} from 'https://deno.land/std/testing/mod.ts';
+import { join } from 'https://deno.land/std/path/mod.ts';
+
+test(async function deployNow() {
+  const proc = Deno.run({
+    args: ['now', '-t', Deno.env()['NOW_TOKEN']],
+    cwd: join(Deno.cwd(), 'example'),
+    stdout: 'piped',
+    stderr: 'piped',
+  });
+  const status = await proc.status();
+  const decoder = new TextDecoder();
+  assert(status.success, decoder.decode(await proc.stderrOutput()));
+  const url = decoder.decode(await proc.output());
+  console.log(`Deployed to ${url}`);
+  const req = await fetch(`${url}/api/version`);
+  assert(req.ok, JSON.stringify(req));
+  const text = await req.text();
+  assertStrContains(text, 'Welcome to deno');
+  assertStrContains(text, '🦕');
+});
