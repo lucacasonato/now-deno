944ff916f69f07723d28f9f4a80e97282edcbb8d
Luca Casonato <luca.casonato@antipy.com>
2020-02-03 23:20:55 +0100

    Added 'now dev' test (#11)
    * Added 'now dev' test
    
    * Cleaned up ci
    
    * Also run deploy on macOS and now dev on windows
    
    * Added 'needs: build'
    
    * Fix tests
    
    * Updated dev timeout
    
    * Maybe windows fix for test
    
    * Install now on windows
    
    * Added bold comment to readme
    
    * Kill subprocess
    
    * Kill with SIGINT 2

--- .github/workflows/tests.ts
+++ .github/workflows/tests.ts
@@ -5,22 +5,57 @@ import {
 } from 'https://deno.land/std/testing/mod.ts';
 import { join } from 'https://deno.land/std/path/mod.ts';
 
-test(async function deployNow() {
-  const proc = Deno.run({
-    args: ['npx', 'now', '-t', Deno.env()['NOW_TOKEN']],
-    cwd: join(Deno.cwd(), 'example'),
-    stdout: 'piped',
-    stderr: 'piped',
-  });
-  const status = await proc.status();
-  const decoder = new TextDecoder();
-  assert(status.success, decoder.decode(await proc.stderrOutput()));
-  const url = decoder.decode(await proc.output());
-  console.log(`Deployed to ${url}`);
-  await new Promise(resolve => setTimeout(resolve, 1000));
-  const req = await fetch(`${url}/api/version`);
-  assert(req.ok);
-  const text = await req.text();
-  assertStrContains(text, 'Welcome to deno');
-  assertStrContains(text, '🦕');
+const isWin = Deno.build.os == 'win';
+const runNow = isWin ? ['now.cmd'] : ['npx', 'now'];
+
+test({
+  name: 'deploy to now',
+  async fn() {
+    const proc = Deno.run({
+      args: runNow.concat('-t', Deno.env()['NOW_TOKEN']),
+      cwd: join(Deno.cwd(), 'example'),
+      stdout: 'piped',
+      stderr: 'piped',
+    });
+    const status = await proc.status();
+    const decoder = new TextDecoder();
+    assert(status.success, decoder.decode(await proc.stderrOutput()));
+    const url = decoder.decode(await proc.output());
+    console.log(`Deployed to ${url}`);
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    const req = await fetch(`${url}/api/version`);
+    assert(req.ok);
+    const text = await req.text();
+    assertStrContains(text, 'Welcome to deno');
+    assertStrContains(text, '🦕');
+  },
 });
+
+if (!isWin) {
+  test({
+    name: 'run on now dev',
+    async fn() {
+      const proc = Deno.run({
+        args: runNow.concat('dev'),
+        cwd: join(Deno.cwd(), 'example'),
+        stdout: 'inherit',
+        stderr: 'inherit',
+      });
+      for (let i = 0; i < 20; i++) {
+        try {
+          const req = await fetch(`http://localhost:3000/api/version`);
+          if (req.ok) {
+            const text = await req.text();
+            assertStrContains(text, 'Welcome to deno');
+            assertStrContains(text, '🦕');
+            proc.kill(2);
+            return;
+          }
+        } catch (err) {}
+        await new Promise(resolve => setTimeout(resolve, 3000));
+      }
+      proc.kill(2);
+      throw Error('Failed to send request to now dev');
+    },
+  });
+}
