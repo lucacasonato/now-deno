c9d13286a8769c105d44fb9c424eefff77bbf9ec
Luca Casonato <lucacasonato@yahoo.com>
2020-02-08 23:41:38 +0100

    Trigger CI

--- example/api/version.ts
+++ example/api/version.ts
@@ -1,12 +1,13 @@
 import {
   APIGatewayProxyEvent,
-  Context
+  APIGatewayProxyResult,
+  Context,
 } from 'https://deno.land/x/lambda/mod.ts';
 
 export async function handler(
   event: APIGatewayProxyEvent,
   context: Context
-) {
+): Promise<APIGatewayProxyResult> {
   return {
     statusCode: 200,
     body: `Welcome to deno ${Deno.version.deno} 🦕`,
