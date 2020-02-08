c407cc2fd24cbd1e9b1d6fae5183e4a88d5ea335
Andy Hayden <andyhayden1@gmail.com>
2020-02-08 14:46:28 -0800

    Use Context rather than APIGatewayEventRequestContext (#13)
    * Use Context rather than APIGatewayEventRequestContext
    
    Note: APIGatewayEvent is deprecated and APIGatewayProxyEvent should be preferred.
    
    I also added the APIGatewayProxyResult type to the handler as I think this is
    useful to have in the example - it will fail to compile if the returned
    value is missing attributes.
    
    APIGatewayEventRequestContext is available on event.requestContext, it's not the
    second argument for the handler.
    
    * Trigger CI
    
    * Also trigger CI on PR
    
    * Revert last commit
    
    Co-authored-by: Luca Casonato <luca.casonato@antipy.com>

--- example/api/version.ts
+++ example/api/version.ts
@@ -1,12 +1,13 @@
 import {
-  APIGatewayEventRequestContext,
-  APIGatewayEvent,
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
 } from 'https://deno.land/x/lambda/mod.ts';
 
 export async function handler(
-  event: APIGatewayEvent,
-  context: APIGatewayEventRequestContext
-) {
+  event: APIGatewayProxyEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> {
   return {
     statusCode: 200,
     body: `Welcome to deno ${Deno.version.deno} 🦕`,
